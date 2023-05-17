f19670eefca8ad8c9e04b081661ebfff41beed06
Mr.Tom <127591461+MrTom-js16@users.noreply.github.com>
2023-05-17 19:58:24 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['0788233416', 'sandaru', true],
+  ['0788233416', 'TOM', true], 
   [''] 
 ] //Number of owners
 
